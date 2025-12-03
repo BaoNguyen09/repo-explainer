c34e1ca65ab29443144d676db57d1d9f17efdc96
BaoNguyen09 <nttbvlogs@gmail.com>
2025-12-03 14:32:08 -0700

    feat: implement ClaudeService for API interactions and add environment variable for API key

---
+++ backend/claude_service.py
@@ -0,0 +1,63 @@
+import anthropic
+import env, utils
+from schema import RepoInfo
+
+__all__ = ["ClaudeService"]
+
+class ClaudeService:
+    """Handles all Claude API interactions"""
+    
+    MODEL = "claude-haiku-4-5-20251001"
+    
+    @classmethod
+    async def explain_repo(
+        cls,
+        repo: RepoInfo,
+        repo_context: str,
+    ) -> tuple[str, bool]:
+        """
+        Send repo context to Claude and get explanation.
+        
+        Args:
+            repo: info related to the requested repo to fetch
+            repo_context: The formatted context from GitHubTools.get_repo_context()
+            
+        Returns:
+            explanation: The generated explanation
+            success: Boolean status
+        """
+
+        SYSTEM_PROMPT = """You are an expert software engineer. Explain GitHub repositories 
+        clearly and concisely for developers who want to understand what a codebase does 
+        and how it works. Produce answer in Markdown format."""
+
+        USER_PROMPT_TEMPLATE = """Explain this repository: {repo_name}
+
+        Repository context:
+        {repo_context}"""
+        repo_name = f"{repo.owner}/{repo.repo_name}"
+        prompt = USER_PROMPT_TEMPLATE.format(repo_name=repo_name, repo_context=repo_context)
+
+        try:
+            utils.logger.info(f"{cls.__name__}.explain_repo(): Set up Claude client")
+            client = anthropic.AsyncAnthropic(api_key=env.ANTHROPIC_API_KEY)
+
+            utils.logger.info(f"{cls.__name__}.explain_repo(): Calling Claude api")
+            message = await client.messages.create(
+                model=cls.MODEL,
+                max_tokens=10_000,
+                system=SYSTEM_PROMPT,
+                messages=[{
+                    "role": "user", 
+                    "content": prompt,
+
+                }]
+            )
+
+            return message.content[0].text, True
+            
+        except Exception as e:
+            utils.logger.error(f"{cls.__name__}.explain_repo(): {e}")
+            return str(e), False
+
+    
--- backend/env.py
+++ backend/env.py
@@ -8,3 +8,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LOGGER_LEVEL: str = os.environ.get("LOG_LEVEL", "INFO")
+ANTHROPIC_API_KEY: str = os.environ.get("ANTHROPIC_API_KEY", "")

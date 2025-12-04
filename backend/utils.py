d38d3941532dfd4403306f8f0339509f6dac1a39
BaoNguyen09 <nttbvlogs@gmail.com>
2025-12-04 16:50:09 -0700

    feat: add date_now function for timezone-aware datetime

--- backend/utils.py
+++ backend/utils.py
@@ -1,10 +1,17 @@
+from datetime import datetime
 import logging
+from zoneinfo import ZoneInfo
 import env
 
 __all__ = [
+    "date_now",
     "logger"
 ]
 # Set up logging configuration
 _level = logging._nameToLevel.get(env.LOGGER_LEVEL, "INFO")
 logging.basicConfig(level=_level)
 logger = logging.getLogger(__name__)
+
+def date_now(timezone_name: str = env.TZ) -> datetime:
+    """Creates a TZ-aware instance of datetime.now()"""
+    return datetime.now(ZoneInfo(timezone_name))

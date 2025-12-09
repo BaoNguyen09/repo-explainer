ac279c95333aacc2ef2b38321f2e54eef27f3d97
BaoNguyen09 <nttbvlogs@gmail.com>
2025-12-09 00:42:10 -0700

    fix: use utc timezone for timestamp

--- backend/utils.py
+++ backend/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 import logging
 from backend import env
 
@@ -11,6 +11,6 @@ _level = logging._nameToLevel.get(env.LOGGER_LEVEL, "INFO")
 logging.basicConfig(level=_level)
 logger = logging.getLogger(__name__)
 
-def date_now(timezone_name: str = env.TZ) -> datetime:
+def date_now() -> datetime:
     """Creates a TZ-aware instance of datetime.now()"""
-    return datetime.now()
+    return datetime.now(tz=timezone.utc)
